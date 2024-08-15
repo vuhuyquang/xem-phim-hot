976945efe2a3ce8d9a28e109d16ae2b6488eedac
quangvh18 <quangvh18@ghtk.co>
2024-08-15 19:54:52 +0700

    update code

--- src/components/common/Header/index.vue
+++ src/components/common/Header/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="w-screen h-[64px]">
-    Header
+  <div class="fixed top-0 left-0 w-full bg-white shadow-md flex items-center py-2 h-[64px] text-sm">
+    <div class="container mx-auto max-w-[1336px] flex justify-between">
+      <div>Logo</div>
+      <div>Navigation</div>
+    </div>
   </div>
 </template>
 
